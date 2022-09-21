4e82cf6a6306d85edd62a68ac506c1c8d62771e2
fedepaloz <palozzifederico@gmail.com>
2022-09-21 18:13:34 +0200

    comic controller, comic address, comic index

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.main')
+
+@section('menu-content')
+
+<header>
+    <h1>
+        <a href="{{route('comics.index')}}">lista fumetti</a>
+    </h1>
+</header>
     
-</body>
-</html>
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use Faker\Guesser\Name;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/','HomeController@index')->name('home');
+
+Route::get('/comics','ComicController@index')->name('comics.index');
