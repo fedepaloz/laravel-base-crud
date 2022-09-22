72a7d6806f2d5f6f71aca03b4230855d212a8b0f
fedepaloz <palozzifederico@gmail.com>
2022-09-22 17:43:10 +0200

    edit

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -8,26 +8,28 @@
 
 <form action="{{route('comics.store')}}" method="POST">
     @csrf
+    @method('PUT')
+
     <label for="title">title:</label><br>
-    <input type="text" id="title" name="title" value=""><br>
+    <input type="text" id="title" name="title" value="{{ $comic->title }}"><br>
     
     <label for="description">description:</label><br>
-    <input type="text" id="description" name="description" value=""><br>
+    <input type="text" id="description" name="description" value="{{ $comic->description }}"><br>
     
     <label for="thumb">thumb:</label><br>
-    <input type="text" id="thumb" name="thumb" value=""><br>
+    <input type="text" id="thumb" name="thumb" value="{{ $comic->thumb }}"><br>
     
     <label for="price">price:</label><br>
-    <input type="number" id="price" name="price" value=""><br>
+    <input type="number" id="price" name="price" value="{{ $comic->price }}"><br>
     
     <label for="series">series:</label><br>
-    <input type="text" id="series" name="series" value=""><br>
+    <input type="text" id="series" name="series" value="{{ $comic->series }}"><br>
     
     <label for="sale_date">sale_date:</label><br>
-    <input type="text" id="sale_date" name="sale_date" value=""><br>
+    <input type="text" id="sale_date" name="sale_date" value="{{ $comic->sale_date }}"><br>
     
     <label for="type">type:</label><br>
-    <input type="text" id="type" name="type" value=""><br><br>
+    <input type="text" id="type" name="type" value="{{ $comic->type }}"><br><br>
     
     <input type="submit" value="Submit">
   </form> 
